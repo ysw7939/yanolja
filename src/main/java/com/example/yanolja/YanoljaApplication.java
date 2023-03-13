ba4b23799cc32b3f8a8342c37c99ad0e16144a22
Yang <ysw7939@gmail.com>
2023-03-13 20:11:43 +0900

    Initial commit

---
+++ src/main/java/com/example/yanolja/YanoljaApplication.java
@@ -0,0 +1,13 @@
+package com.example.yanolja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanoljaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanoljaApplication.class, args);
+	}
+
+}
